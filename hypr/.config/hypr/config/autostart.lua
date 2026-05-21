5c80f69014d8bcb583c3cadc94ac0930f7520896
Cyb3rKun <cyb0rg@duck.com>
2026-05-21 23:49:24 +0200

    moved some links and comments

--- hypr/.config/hypr/config/autostart.lua
+++ hypr/.config/hypr/config/autostart.lua
@@ -1,3 +1,8 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
 hl.on("hyprland.start", function()
 	hl.exec_cmd "quickshell"
 	hl.exec_cmd "mako"
