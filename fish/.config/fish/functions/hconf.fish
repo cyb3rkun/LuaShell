b9c2cc4df87392e87a5deaaf8bc2ddfc1e839c34
Cyb3rKun <cyb0rg@duck.com>
2026-05-21 17:01:57 +0200

    fix unescaped string in hconf fish alias/function

--- fish/.config/fish/functions/hconf.fish
+++ fish/.config/fish/functions/hconf.fish
@@ -1,4 +1,4 @@
-function hconf --wraps='cd ~/.config/hypr' --description 'alias hconf=cd ~/.config/hypr
+function hconf --wraps='cd ~/.config/hypr' --description 'alias hconf=cd ~/.config/hypr'
 	cd ~/.config/hypr $argv
 
 end
