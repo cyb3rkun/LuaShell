2340c102bddbe7abb55697707ab4b9cdc7b8a452
Cyb3rKun <cyb0rg@duck.com>
2026-05-21 17:02:50 +0200

    set platformtheme, and add dbus-update to hyprland.starte event

--- hypr/.config/hypr/config/env.lua
+++ hypr/.config/hypr/config/env.lua
@@ -6,7 +6,7 @@ hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 -- use qt5ct for compatibility. make sure both qt6ct and qt5ct and
 -- qt5-wayland, qt6-wayland and finally qt6-5compat are installed
-hl.env("QT_QPA_PLATFRORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFRORMTHEME", "qt6ct")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
@@ -21,3 +21,10 @@ hl.env("TERM", "wezterm")
 
 hl.env("MOZ_ENABLE_WAYLAND", "1")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+
+hl.on(
+	"hyprland.start",
+	function()
+		hl.exec_cmd "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+	end
+)
