e0fae7d385e392c8383cd1146ba9f953dca2ada9
Cyb3rKun <cyb0rg@duck.com>
2026-05-21 17:02:10 +0200

    add hyprpaper to autostarts in hyprland

--- hypr/.config/hypr/config/autostart.lua
+++ hypr/.config/hypr/config/autostart.lua
@@ -1,6 +1,7 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd "quickshell"
 	hl.exec_cmd "mako"
+	hl.exec_cmd "hyprpaper"
 	hl.dispatch(hl.dsp.focus { workspace = 4 })
 	hl.dispatch(hl.dsp.focus { workspace = 1 })
 end)
