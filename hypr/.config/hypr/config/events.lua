1530a554ca07fea5f5fac2bd3b9be9969d88007b
Cyb3rKun <cyb0rg@duck.com>
2026-05-23 01:45:46 +0200

    move autostarts to events.lua

---
+++ hypr/.config/hypr/config/events.lua
@@ -0,0 +1,11 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+hl.on("hyprland.start", function()
+	hl.exec_cmd "quickshell"
+	hl.exec_cmd "mako"
+	hl.exec_cmd "hyprpaper"
+end)
+
