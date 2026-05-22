04655a6411f663bbe1748385d9835dd5b2b00954
Cyb3rKun <cyb0rg@duck.com>
2026-05-23 01:45:35 +0200

    format

--- hypr/.config/hypr/config/binds/spaces.lua
+++ hypr/.config/hypr/config/binds/spaces.lua
@@ -1,9 +1,8 @@
 -- Special Workspaces
 
-	hl.bind(
-		SUPR .. ALT .. "X",
-		hl.dsp.workspace.toggle_special("games"),
-		{
-			desc = "Toggle Gaming Workspace"
-		}
-	)
+hl.bind(SUPR .. ALT .. "X", hl.dsp.workspace.toggle_special "games", {
+	desc = "Toggle Gaming Workspace",
+})
+hl.bind(SUPR .. ALT .. "L", hl.dsp.focus { workspace = "name:Laun" }, {
+	desc = "Toggle Game Launcher Workspace",
+})
