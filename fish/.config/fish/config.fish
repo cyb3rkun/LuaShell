743e06ae4d07f6622bdbde47850323341d35c43f
Cyb3rKun <cyb0rg@duck.com>
2026-05-22 14:35:03 +0200

    add delay before running fastfetch to avoid collisions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,13 @@
-function fish_greeting
-	# smth smth
-end
-
 set -gx QT_QPA_PLATFORMTHEME qt6ct
 
+fish_add_path ~/.local/bin
+
 zoxide init fish | source
+if status is-interactive
+	function fish_greeting
+		# fastfetch
+		# smth smth
+	end
+	sleep 0.1
+	fastfetch
+end
