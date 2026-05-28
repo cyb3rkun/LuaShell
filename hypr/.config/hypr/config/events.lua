fe1552130ab78a036d366687e4463f6ed1f9107c
Cyb3rKun <cyb0rg@duck.com>
2026-05-28 03:15:15 +0200

    add dbus activation environment update to hyprland start

--- hypr/.config/hypr/config/events.lua
+++ hypr/.config/hypr/config/events.lua
@@ -4,9 +4,9 @@
 -- Or execute your favorite apps at launch like this:
 -- AutoStart
 hl.on("hyprland.start", function()
+	hl.exec_cmd "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+	hl.exec_cmd "uwsm app -s b -- nm-applet --indicator"
 	hl.exec_cmd "quickshell"
 	hl.exec_cmd "mako"
 	hl.exec_cmd "hyprpaper"
-	hl.exec_cmd "uwsm app -s b -- nm-applet --indicator"
 end)
-
