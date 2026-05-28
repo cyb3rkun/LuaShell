663d087b4513c47f7f6cd37b5261a8f1850b6000
Cyb3rKun <cyb0rg@duck.com>
2026-05-28 03:16:16 +0200

    modify move_all_win dispatcher

--- hypr/.config/hypr/dsp/init.lua
+++ hypr/.config/hypr/dsp/init.lua
@@ -5,8 +5,12 @@ M.win = {}
 ---@param dest HL.WorkspaceSelector
 ---@param follow boolean # Whether or not the focus should follow the windows.
 ---@return function # The custom dispatcher closure to be executed by Hyprland
+---NOTE: It might be possible to preserve layout using HL.Window.at, but
+--- it would be inconvenient to handle different monitor transforms,
+--- aspect ratios and resolutions
 function M.win.move_all_win(source, dest, follow)
 	return function()
+		---@type HL.Workspace?
 		local source_ws = hl.get_workspace(source)
 		if not source_ws then
 			hl.notification.create {
@@ -17,7 +21,9 @@ function M.win.move_all_win(source, dest, follow)
 			return
 		end
 
+		---@type HL.Window[]?
 		local windows = source_ws:get_windows()
+		if not windows then return end
 
 		for _, window in ipairs(windows) do
 			hl.dispatch(hl.dsp.window.move {
