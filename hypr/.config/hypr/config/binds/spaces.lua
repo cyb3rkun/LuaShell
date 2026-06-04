95ec314abe11b12778071cd4f47883844daff5d9
Cyb3rKun <cyb0rg@duck.com>
2026-06-04 19:23:26 +0200

    change binding for toggling special:games to be consistent with other keymaps for special workspaces

--- hypr/.config/hypr/config/binds/spaces.lua
+++ hypr/.config/hypr/config/binds/spaces.lua
@@ -40,7 +40,7 @@ hl.bind(
 	}
 )
 
-hl.bind(SUPR .. ALT .. "X", hl.dsp.workspace.toggle_special "games", {
+hl.bind(SUPR .. CTL .. "X", hl.dsp.workspace.toggle_special "games", {
 	desc = "Toggle Gaming Workspace",
 })
 hl.bind(SUPR .. CTL .. "L", hl.dsp.focus { workspace = "name:Laun" }, {
